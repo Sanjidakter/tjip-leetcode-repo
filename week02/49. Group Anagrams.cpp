2489b6b8abadb32a9afe31dd8314c9e784195a1b
Sanjida Akter <90788561+Sanjidakter@users.noreply.github.com>
2022-05-18 13:28:38 +0600

    Update week02/49. Group Anagrams.cpp
    Co-authored-by: Md Sadman Sakib <sakib.csedu21@gmail.com>

--- week02/49. Group Anagrams.cpp
+++ week02/49. Group Anagrams.cpp
@@ -5,12 +5,13 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         unordered_map<string,vector<string>>umap;
-        for(auto x:strs)
+        for(auto str : strs)
         {
-            string temp = x;
-            sort(x.begin(),x.end());
-            umap[x].push_back(temp);
+            string sorted = str;
+            sort(sorted.begin(), sorted.end());
+            umap[sorted].push_back(str);
         }
+
         for(auto x:umap)
         {
             ans.push_back(x.second);
